f24df93447ba1a823f04fdca0db5aa46a1ea3527
Unknown <lomik248@mail.ru>
2017-03-06 20:51:21 +0300

    2

--- src/CoreGit/Startup.cs
+++ src/CoreGit/Startup.cs
@@ -30,7 +30,7 @@ namespace CoreGit
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Wod!");
             });
         }
     }
